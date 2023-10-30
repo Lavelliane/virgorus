4b87a83f413ec82f40c0f3948e6e840902e3845a
Vince Abella <vinceabella05@gmail.com>
2023-10-30 17:51:03 +0800

    temp

--- src/components/TableRates.tsx
+++ src/components/TableRates.tsx
@@ -11,7 +11,10 @@ import {
 	Tooltip,
 	ChipProps,
 	getKeyValue,
+	Button,
+	Input,
 } from '@nextui-org/react';
+import { IoAddCircleOutline } from 'react-icons/io5';
 
 export default function TableRates() {
 	const rows = [{ key: '1' }];
@@ -30,6 +33,37 @@ export default function TableRates() {
 				{(item) => (
 					<TableRow key={item.key}>{(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}</TableRow>
 				)}
+				<TableRow>
+					<TableCell>
+						<Input
+							type='text'
+							value={activity.some((ac) => ac.isEditing) ? '' : newActivity}
+							onChange={(e) => setNewActivity(e.target.value)}
+							placeholder='Activity Name'
+							disabled={activity.some((ac) => ac.isEditing)}
+							className=' sm:text-sm text-xs mx-0'
+						/>
+					</TableCell>
+					<TableCell>
+						<Input
+							type='text'
+							value={activity.some((ac) => ac.isEditing) ? '' : newTimeSlot}
+							onChange={(e) => setNewTimeSlot(e.target.value)}
+							placeholder='Time Slot'
+							disabled={activity.some((ac) => ac.isEditing)}
+							className=' sm:text-sm text-xs mx-0'
+						/>
+					</TableCell>
+					<TableCell className='justify-end flex items-center'>
+						<Button
+							onClick={addActivity}
+							className='p-4 m-0 bg-transparent text-purple-600 hover:text-purple-400 text-xl hover:bg-transparent'
+							disabled={activity.some((ac) => ac.isEditing)}
+						>
+							<IoAddCircleOutline />
+						</Button>
+					</TableCell>
+				</TableRow>
 			</TableBody>
 		</Table>
 	);
