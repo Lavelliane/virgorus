a2190946edf6368fd70f763bab1ab843a002b4e2
Johnfil Initan <njohnfil.pr+github@gmail.com>
2023-11-03 02:36:09 +0800

    Added database GET function for BookingPage

--- src/components/Guest/Booking/PackageDetails.tsx
+++ src/components/Guest/Booking/PackageDetails.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import React from 'react';
+import { useEffect , useState } from 'react'
+import { useMount } from 'react-use'
 import {
 	Accordion,
 	AccordionItem,
@@ -18,18 +20,19 @@ import { BookingForm } from './BookingForm';
 import Recommendations from './Recommendations';
 import { FaExclamationCircle } from 'react-icons/fa';
 import { MdTimelapse, MdCancel, MdLanguage, MdOutlineGroups } from 'react-icons/md';
+import Package from '../../../types/package';
 
-export default function PackageDetails() {
-	const [note, setNote] = React.useState(true);
+
+export default function PackageDetails({ packageData }: { packageData: Package }) {
 	const foo = 'foo';
 	const loremIpsum =
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
-
+	console.log(packageData)
 	return (
 		<div className='flex flex-col'>
 			<div className='flex flex-col'>
 				<Spacer y={10} />
-				<p className='font-bold text-black text-2xl'>{loremIpsum}</p>
+				<p className='font-bold text-black text-2xl'>{packageData.name}</p>
 				<Spacer y={10} />
 				<div>
 					<Gallery />
@@ -40,21 +43,19 @@ export default function PackageDetails() {
 				<div className='w-full'>
 					<div className='w-full text-black text-sm'>
 						<h1 className='font-semibold pb-2 text-lg'>About</h1>
-						{loremIpsum}
+						{packageData.description}
 					</div>
 					<div className='w-full text-black py-2 text-sm'>
 						<h1 className=''>
 							starts at <span className='text-2xl font-bold'>₱{foo}</span> per adult (see rates below for group pricing)
 						</h1>
 					</div>
-					{note ? (
-						<Card className='bg-nude py-2 my-4'>
-							<CardHeader className='font-semibold'>
-								<FaExclamationCircle /> &nbsp;Note:
-							</CardHeader>
-							<CardBody className='text-sm'>{loremIpsum}</CardBody>
-						</Card>
-					) : null}
+					<Card className='bg-nude py-2 my-4' hidden={!packageData.notice}>
+						<CardHeader className='font-semibold'>
+							<FaExclamationCircle /> &nbsp;Note:
+						</CardHeader>
+						<CardBody className='text-sm'>{packageData.notice}</CardBody>
+					</Card>
 					<Divider />
 					<div className='flex flex-col py-4'>
 						<div>
@@ -74,7 +75,7 @@ export default function PackageDetails() {
 									<span className='text-lg flex items-center'>
 										<MdTimelapse />
 									</span>
-									<span className='text-md flex items-center'>Duration: {foo}</span>
+									<span className='text-md flex items-center'>Duration: {packageData.duration}</span>
 								</Button>
 							</Tooltip>
 						</div>
@@ -86,7 +87,7 @@ export default function PackageDetails() {
 									<div className='px-1 py-2'>
 										<div className='text-small font-bold text-black'>Cancellation</div>
 										<div className='text-tiny text-black w-48 text-justify'>
-											For a full refund, cancel at least 48 hours in advance of the start date of the experience.
+											For a full refund, cancel at least {packageData.cancellation} in advance of the start date of the experience.
 										</div>
 									</div>
 								}
@@ -95,7 +96,7 @@ export default function PackageDetails() {
 									<span className='text-lg flex items-center'>
 										<MdCancel />
 									</span>
-									<span className='text-md flex items-center'>48-hour cancellation</span>
+									<span className='text-md flex items-center'>Cancellation: {packageData.cancellation}</span>
 								</Button>
 							</Tooltip>
 						</div>
@@ -137,7 +138,7 @@ export default function PackageDetails() {
 									<span className='text-lg flex items-center'>
 										<MdLanguage />
 									</span>
-									<span className='text-md flex items-center'>Live guide: {foo}</span>
+									<span className='text-md flex items-center'>Live guide: {packageData.language}</span>
 								</Button>
 							</Tooltip>
 						</div>
@@ -153,13 +154,13 @@ export default function PackageDetails() {
 							}}
 						>
 							<AccordionItem title='Rates and inclusions' className='text-black'>
-								{loremIpsum}
+								{packageData.inclusions}
 							</AccordionItem>
 							<AccordionItem title='Sample itinerary' className='text-black'>
 								{loremIpsum}
 							</AccordionItem>
 							<AccordionItem title='What to expect' className='text-black'>
-								{loremIpsum}
+								{packageData.expectations}
 							</AccordionItem>
 							<AccordionItem title='Accessibility' className='text-black'>
 								{loremIpsum}
