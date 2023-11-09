fb0512484f2c7ace70d3604d9c418d3884a9b4e3
Vince Abella <vinceabella05@gmail.com>
2023-11-09 09:10:18 +0800

    removed expectations in api

--- src/app/api/package/route.ts
+++ src/app/api/package/route.ts
@@ -1,75 +1,72 @@
-import { PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
 interface RatesAndInclusions {
-  numberOfPax: string;
-  ratePerPax: string;
+	numberOfPax: string;
+	ratePerPax: string;
 }
 
 interface Itinerary {
-  time?: string;
-  activity?: string;
+	time?: string;
+	activity?: string;
 }
 
 export async function POST(req: NextRequest) {
-  const packageData = await req.json();
-  const createdPackage = await prisma.package.create({
-    data: {
-      name: packageData.name,
-      description: packageData.description,
-      type: packageData.type,
-      location: packageData.location,
-      duration: packageData.duration,
-      cancellation: packageData.cancellation,
-      availability: packageData.availability,
-      language: packageData.language,
-      inclusions: { set: packageData.inclusions },
-      exclusions: { set: packageData.exclusions },
-      notice: packageData.notice,
-      ratesAndInclusions: {
-        create: packageData.ratesAndInclusions.map(
-          (ratesAndInclusion: RatesAndInclusions) => {
-            return {
-              numberOfPax: ratesAndInclusion.numberOfPax,
-              ratePerPax: ratesAndInclusion.ratePerPax,
-            };
-          }
-        ),
-      },
-      itinerary: {
-        create: packageData.itinerary.map((itinerary: Itinerary) => {
-          return {
-            time: itinerary.time,
-            activity: itinerary.activity,
-          };
-        }),
-      },
-      expectations: { set: packageData.expectations },
-      photos: { set: packageData.photos },
-    },
-  });
-  return NextResponse.json(createdPackage, { status: 200 });
+	const packageData = await req.json();
+	const createdPackage = await prisma.package.create({
+		data: {
+			name: packageData.name,
+			description: packageData.description,
+			type: packageData.type,
+			location: packageData.location,
+			duration: packageData.duration,
+			cancellation: packageData.cancellation,
+			availability: packageData.availability,
+			language: packageData.language,
+			inclusions: { set: packageData.inclusions },
+			exclusions: { set: packageData.exclusions },
+			notice: packageData.notice,
+			ratesAndInclusions: {
+				create: packageData.ratesAndInclusions.map((ratesAndInclusion: RatesAndInclusions) => {
+					return {
+						numberOfPax: ratesAndInclusion.numberOfPax,
+						ratePerPax: ratesAndInclusion.ratePerPax,
+					};
+				}),
+			},
+			itinerary: {
+				create: packageData.itinerary.map((itinerary: Itinerary) => {
+					return {
+						time: itinerary.time,
+						activity: itinerary.activity,
+					};
+				}),
+			},
+			photos: { set: packageData.photos },
+		},
+	});
+	return NextResponse.json(createdPackage, { status: 200 });
 }
 
 export async function GET(req: NextRequest) {
-  try {
-    const packages = await prisma.package.findMany({
-      include: {
-        ratesAndInclusions: true,
-        itinerary: {
-          include: {
-            itineraries: true,
-          },
-        },
-      },
-    });
-    return NextResponse.json(packages, { status: 200 })
-  } catch (error) {
-    console.error(error)
-    return NextResponse.json(error, { status: 500 })
-  } finally {
-    await prisma.$disconnect();
-  }
+	try {
+		const packages = await prisma.package.findMany({
+			include: {
+				ratesAndInclusions: true,
+				itinerary: {
+					include: {
+						itineraries: true,
+					},
+				},
+			},
+		});
+		return NextResponse.json(packages, { status: 200 });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(error, { status: 500 });
+	} finally {
+		await prisma.$disconnect();
+	}
 }
--- src/components/Admin/EditPackage.tsx
+++ src/components/Admin/EditPackage.tsx
@@ -26,6 +26,7 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchPackage } from '@/queries/fetchPackages';
 
 async function editPackage(data: IAddPackage, id: any) {
+	console.log(data);
 	try {
 		const response = await fetch(`/api/package/${id}`, {
 			method: 'PATCH',
@@ -37,16 +38,15 @@ async function editPackage(data: IAddPackage, id: any) {
 
 		if (response.ok) {
 			const updatedPackage = await response.json();
-			return updatedPackage; // Return the updated data
+			return updatedPackage;
 		} else {
 			const errorData = await response.json();
 			console.error('Failed to update package:', errorData);
-			// You can throw an error here or handle the error as needed
 			throw new Error(`Failed to update package: ${response.statusText}`);
 		}
 	} catch (error) {
 		console.error('An error occurred while updating package:', error);
-		throw error; // Re-throw the error for higher-level error handling
+		throw error;
 	}
 }
 
@@ -88,8 +88,7 @@ export default function EditPackage({ id }: Props) {
 		}
 	}, [packageLoading, packageData]);
 
-	console.log(form);
-	const handleActionClick = () => {
+	const handleSaveClick = () => {
 		editPackage(form, packageData.id);
 	};
 
@@ -123,169 +122,161 @@ export default function EditPackage({ id }: Props) {
 		setForm({ ...form, [e.target.name]: e.target.value });
 	};
 
-	console.log(form);
 	return (
 		<Card>
-			<form onSubmit={handleActionClick}>
-				<CardHeader className='flex flex-col gap-1'>Edit Package</CardHeader>
-				<CardBody>
-					<div className='flex flex-col gap-4'>
-						<div className='flex gap-4'>
-							<Input
-								value={form?.name || ''}
-								className='w-[50%]'
-								name='name'
-								onChange={onChange}
-								type='text'
-								size='sm'
-								labelPlacement='outside'
-								label='Package Name'
-								placeholder='Enter package name'
-								isRequired
-							/>
-							<Input
-								value={form?.type}
-								className='w-[25%]'
-								name='type'
-								onChange={onChange}
-								type='text'
-								size='sm'
-								labelPlacement='outside'
-								label='Type'
-								placeholder='Enter type'
-								isRequired
-							/>
-							<Input
-								name='location'
-								className='w-[25%]'
-								value={form?.location}
-								onChange={onChange}
-								type='text'
-								size='sm'
-								labelPlacement='outside'
-								label='Location'
-								placeholder='Enter location'
-								isRequired
-							/>
-						</div>
-						<div className='flex flex-col gap-4'>
-							<Textarea
-								className='w-[100%]'
-								name='description'
-								value={form?.description}
-								onChange={onChange}
-								type='text'
-								size='sm'
-								labelPlacement='outside'
-								label='Description'
-								placeholder='Enter package description'
-								isRequired
-								minRows={10}
-							/>
-							<Textarea
-								className='w-[100%]'
-								name='notice'
-								value={form?.notice}
-								onChange={onChange}
-								type='text'
-								size='sm'
-								labelPlacement='outside'
-								label='Notice'
-								placeholder='Enter notice'
-								minRows={4}
-							/>
-						</div>
-						<div className='flex gap-4'>
-							<Input
-								value={form?.duration}
-								className='w-[50%]'
-								name='duration'
-								onChange={onChange}
-								type='text'
-								size='sm'
-								min='1'
-								labelPlacement='outside'
-								label='Package Duration (in hours)'
-								placeholder='i.e. 12, 8-10'
-								isRequired
-							/>
-							<Tooltip
-								placement='top'
-								delay={500}
-								closeDelay={100}
-								content="Leave empty for 'No cancellation' policies."
-							>
-								<Input
-									value={form?.cancellation}
-									className='w-[50%]'
-									name='cancellation'
-									onChange={onChange}
-									type='text'
-									size='sm'
-									labelPlacement='outside'
-									label='Cancellation Policy (in hours)'
-									placeholder='i.e. 24, 48'
-									isRequired
-								/>
-							</Tooltip>
-						</div>
-						<div className='flex gap-4'>
-							<Select
-								label='Availability'
-								labelPlacement='outside'
-								selectionMode='multiple'
-								placeholder='Select Availability'
-								selectedKeys={availability}
-								size='sm'
-								className='w-[50%]'
-								onChange={availabilitySelectionChange}
-							>
-								{availabilityData?.map((availability) => (
-									<SelectItem key={availability.value} value={availability.value}>
-										{availability.label}
-									</SelectItem>
-								))}
-							</Select>
-							<Select
-								label='Language'
-								labelPlacement='outside'
-								selectionMode='multiple'
-								placeholder='Select Language'
-								selectedKeys={language}
-								size='sm'
-								className='w-[50%]'
-								onChange={languageSelectionChange}
-							>
-								{languagesData?.map((language) => (
-									<SelectItem key={language.name} value={language.name}>
-										{language.name}
-									</SelectItem>
-								))}
-							</Select>
-						</div>
-						<Divider className='my-0' />
-						<div className='flex flex-row gap-4'>
-							<div className='flex w-full gap-4'>
-								<TableRates onChange={onChange} form={form} />
-							</div>
-							<div className='flex flex-col w-full gap-4'>
-								<TableInclusions onChange={onChange} form={form} />
-								<TableExclusions onChange={onChange} form={form} />
-							</div>
-						</div>
-						<Divider className='my-0' />
-						<TableItinerary onChange={onChange} form={form} />
+			<CardHeader className='flex flex-col gap-1'>Edit Package</CardHeader>
+			<CardBody>
+				<div className='flex flex-col gap-4'>
+					<div className='flex gap-4'>
+						<Input
+							value={form?.name || ''}
+							className='w-[50%]'
+							name='name'
+							onChange={onChange}
+							type='text'
+							size='sm'
+							labelPlacement='outside'
+							label='Package Name'
+							placeholder='Enter package name'
+							isRequired
+						/>
+						<Input
+							value={form?.type}
+							className='w-[25%]'
+							name='type'
+							onChange={onChange}
+							type='text'
+							size='sm'
+							labelPlacement='outside'
+							label='Type'
+							placeholder='Enter type'
+							isRequired
+						/>
+						<Input
+							name='location'
+							className='w-[25%]'
+							value={form?.location}
+							onChange={onChange}
+							type='text'
+							size='sm'
+							labelPlacement='outside'
+							label='Location'
+							placeholder='Enter location'
+							isRequired
+						/>
 					</div>
-				</CardBody>
-				<CardFooter>
-					<Button color='danger' variant='light' onPress={handleReturn}>
-						Return
-					</Button>
-					<Button color='secondary' type='submit'>
-						Save
-					</Button>
-				</CardFooter>
-			</form>
+					<div className='flex flex-col gap-4'>
+						<Textarea
+							className='w-[100%]'
+							name='description'
+							value={form?.description}
+							onChange={onChange}
+							type='text'
+							size='sm'
+							labelPlacement='outside'
+							label='Description'
+							placeholder='Enter package description'
+							isRequired
+							minRows={10}
+						/>
+						<Textarea
+							className='w-[100%]'
+							name='notice'
+							value={form?.notice}
+							onChange={onChange}
+							type='text'
+							size='sm'
+							labelPlacement='outside'
+							label='Notice'
+							placeholder='Enter notice'
+							minRows={4}
+						/>
+					</div>
+					<div className='flex gap-4'>
+						<Input
+							value={form?.duration}
+							className='w-[50%]'
+							name='duration'
+							onChange={onChange}
+							type='text'
+							size='sm'
+							min='1'
+							labelPlacement='outside'
+							label='Package Duration (in hours)'
+							placeholder='i.e. 12, 8-10'
+							isRequired
+						/>
+						<Tooltip placement='top' delay={500} closeDelay={100} content="Leave empty for 'No cancellation' policies.">
+							<Input
+								value={form?.cancellation}
+								className='w-[50%]'
+								name='cancellation'
+								onChange={onChange}
+								type='text'
+								size='sm'
+								labelPlacement='outside'
+								label='Cancellation Policy (in hours)'
+								placeholder='i.e. 24, 48'
+								isRequired
+							/>
+						</Tooltip>
+					</div>
+					<div className='flex gap-4'>
+						<Select
+							label='Availability'
+							labelPlacement='outside'
+							selectionMode='multiple'
+							placeholder='Select Availability'
+							selectedKeys={availability}
+							size='sm'
+							className='w-[50%]'
+							onChange={availabilitySelectionChange}
+						>
+							{availabilityData?.map((availability) => (
+								<SelectItem key={availability.value} value={availability.value}>
+									{availability.label}
+								</SelectItem>
+							))}
+						</Select>
+						<Select
+							label='Language'
+							labelPlacement='outside'
+							selectionMode='multiple'
+							placeholder='Select Language'
+							selectedKeys={language}
+							size='sm'
+							className='w-[50%]'
+							onChange={languageSelectionChange}
+						>
+							{languagesData?.map((language) => (
+								<SelectItem key={language.name} value={language.name}>
+									{language.name}
+								</SelectItem>
+							))}
+						</Select>
+					</div>
+					<Divider className='my-0' />
+					<div className='flex flex-row gap-4'>
+						<div className='flex w-full gap-4'>
+							<TableRates onChange={onChange} form={form} />
+						</div>
+						<div className='flex flex-col w-full gap-4'>
+							<TableInclusions onChange={onChange} form={form} />
+							<TableExclusions onChange={onChange} form={form} />
+						</div>
+					</div>
+					<Divider className='my-0' />
+					<TableItinerary onChange={onChange} form={form} />
+				</div>
+			</CardBody>
+			<CardFooter>
+				<Button color='danger' variant='light' onPress={handleReturn}>
+					Return
+				</Button>
+				<Button color='secondary' onPress={handleSaveClick}>
+					Save
+				</Button>
+			</CardFooter>
 		</Card>
 	);
 }
