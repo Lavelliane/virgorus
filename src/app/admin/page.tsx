27b99dcfe3177a4b3d08f6f7c3952b2482cb49ed
Vince Abella <vinceabella05@gmail.com>
2023-11-12 01:45:26 +0800

    feature/change location to select component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Dashboard from '@/components/Admin/Dashboard';
 import NavbarAdmin from '@/components/Admin/NavbarAdmin';
 import { SessionProvider } from 'next-auth/react';
 
@@ -9,7 +10,9 @@ export default function AdminPage() {
 			<main className='flex min-h-screen flex-col items-center justify-between bg-white'>
 				<section className='flex flex-col w-full h-fit items-center'>
 					<NavbarAdmin />
-					<div className='flex max-w-5xl py-10'>To add dashboard</div>
+					<div className='flex w-full p-10 '>
+						<Dashboard />
+					</div>
 				</section>
 			</main>
 		</SessionProvider>
