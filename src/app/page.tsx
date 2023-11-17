515c9dff70e98ac2c6343695ed7b70c94016916b
Zach Riane Machacon <zachriane01@gmail.com>
2023-11-17 21:51:54 +0800

    new: added catalog card component and suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-'use client'
+"use client";
 
-import { CatalogCard } from "@/components/Guest/CatalogCard";
+import { Catalog } from "@/components/Guest/Catalog";
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchPackages } from "@/queries/fetchPackages";
-
-type Package = {
-  id: number;
-  name: string;
-  description: string;
-  type: string;
-  location: string;
-};
+import { CatalogPackage } from "@/types/package";
+import NavbarGuest from "@/components/Guest/NavbarGuest";
+import { ContactBar } from "@/components/Guest/ContactBar";
+import SitemapFooter from "@/components/Footer";
+import { CatalogCardSuspense } from "@/components/Guest/CatalogCardSuspense";
 
 export default function Home() {
-  const [packages, setPackages] = useState<Package[]>([]);
+  const [packages, setPackages] = useState<CatalogPackage[]>([]);
   const { data: packagesData, isLoading: packagesLoading } = useQuery({
     queryKey: ["packages"],
     queryFn: fetchPackages,
@@ -22,14 +19,13 @@ export default function Home() {
 
   useEffect(() => {
     if (!packagesLoading && packagesData) {
-      console.log(packagesData);
       setPackages(
         packagesData.map((pd: any) => ({
           id: pd.id,
           name: pd.name,
           description: pd.description,
           type: pd.type,
-          location: pd.location,
+          rate: pd.rates[0].ratePerPax,
         }))
       );
     }
@@ -37,7 +33,22 @@ export default function Home() {
 
   return (
     <>
-      <CatalogCard packages={packages} />
+      <main className="flex flex-col items-center justify-between bg-white h-screen">
+        <section className="flex flex-col w-full h-full items-center">
+          <div className="flex flex-col w-full fixed z-30">
+            <NavbarGuest />
+            <ContactBar />
+          </div>
+          <div className="flex h-full w-full max-w-7xl pt-24 gap-3 my-10">
+            {!packagesLoading ? (
+              <Catalog packages={packages} />
+            ) : (
+              <CatalogCardSuspense />
+            )}
+          </div>
+          <SitemapFooter />
+        </section>
+      </main>
     </>
   );
 }
--- src/components/Guest/CatalogCard.tsx
+++ src/components/Guest/CatalogCard.tsx
@@ -6,25 +6,46 @@ import {
   CardBody,
   CardFooter,
 } from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { fetchPackages } from "@/queries/fetchPackages";
+import { CatalogPackage } from "@/types/package";
+import fallbackImage from "../../../public/fallbackImage.jpg";
+import Image from "next/image";
+import Link from "next/link";
 
-type Package = {
-  id: number;
-  name: string;
-  description: string;
-  type: string;
-  location: string;
-};
-
-export const CatalogCard = (package: Package) => {
+export const CatalogCard = ({ catPackage }: { catPackage: CatalogPackage }) => {
+  console.log(catPackage);
   return (
     <>
-      <Card>
-        <CardHeader>{package.type}</CardHeader>
-        <CardBody>Hi</CardBody>
-        <CardFooter></CardFooter>
+      <Card className="max-w-5xl">
+        <CardHeader className="flex flex-col items-start gap-2">
+          <span className="font-bold text-gray-400 uppercase">
+            {catPackage.type}
+          </span>
+          <span className="font-bold text-2xl">{catPackage.name}</span>
+          <span className="text-lg">{catPackage.description}</span>
+        </CardHeader>
+        <CardBody>
+          <Image
+            src={fallbackImage}
+            alt="Picture of the tour"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+        </CardBody>
+        <CardFooter className="flex justify-between gap-2">
+          <div className="flex flex-col">
+            <span className="text-md">Starts at</span>
+            <div className="flex gap-1">
+              <span className="text-md text-olive">₱ {catPackage.rate}.00</span>
+              <span className="text-md"> / person</span>
+            </div>
+          </div>
+          <Link href={`/tours/${catPackage.id}`}>
+            <Button radius="full" className="bg-chocolate text-white">
+              Book Now
+            </Button>
+          </Link>
+        </CardFooter>
       </Card>
     </>
   );
